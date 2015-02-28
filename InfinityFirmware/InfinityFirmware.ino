1ace4579c6211a5e349454455c64fc6da5acc482
Adam Tindale <adam@adamtindale.com>
2015-02-28 15:59:56 -0500

    fixing toggle and cleaning up globals

--- InfinityFirmware/InfinityFirmware.ino
+++ InfinityFirmware/InfinityFirmware.ino
@@ -9,9 +9,11 @@
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(12, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(13, PIN, NEO_GRB + NEO_KHZ800);
 
-#define buttonPin 4
+#define MAX_BEAN_SLEEP 0xFFFFFFFF
+
+#define NEOPIXELRINGLENGTH 12
 
 // The control inputs we will use from LightBlue iOS app
 #define button1  13
@@ -21,6 +23,21 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(12, PIN, NEO_GRB + NEO_KHZ800);
 #define button5  17
 #define slider29 29
 
+// the number of the pushbutton pin
+#define buttonPin 4
+#define buzzpin 0
+
+int buttonState = HIGH;  // current reading from the button
+int activityLevel = 6;
+
+int previous = HIGH;    // the previous reading from the input pin
+int toggleState = HIGH;
+
+// the follow variables are long's because the time, measured in miliseconds,
+// will quickly become a bigger number than can be stored in an int.
+long time = 0;         // the last time the output pin was toggled
+long debounce = 200;   // the debounce time, increase if the output flickers
+
 void setup() 
 {
   Serial.begin(57600);
@@ -29,10 +46,9 @@ void setup()
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
 
-  pinMode(0, OUTPUT);
+  pinMode(buzzpin, OUTPUT); // 
+  
   pinMode(buttonPin, INPUT_PULLUP);
-
-
   Bean.attachChangeInterrupt(buttonPin, togglelights);
 }
 
@@ -42,7 +58,8 @@ void loop() {
   size_t length = 64; 
 
   length = Serial.readBytes(buffer, 2);    
-  
+  strip.show();
+
   if ( length > 0 )
   {
     byte controlbyte = buffer [0];
@@ -81,30 +98,41 @@ void loop() {
     strip.show();
   }
 
-  //Bean.sleep(500);
+  Bean.sleep(100);
 }
 
 void party()
 {
+  
+    Bean.setLed(100,100,100);
     int buzzState = LOW;
-    for(int i = 0; i < strip.numPixels(); i++)
+    
+    
+    for(int i = 0; i < 12; i++)
     {
-        digitalWrite();
+        strip.setPixelColor(i,  strip.Color(100,0,0) );
+        strip.setPixelColor(12, strip.Color(100*buzzState,0,0) );
+        digitalWrite(0, buzzState);
+        buzzState = !buzzState;
+        Bean.sleep(100);
     }
-     
+    
+    digitalWrite(0, LOW);
+    Bean.setLed(0,0,0);
 }
 
 void togglelights()
 {
   buttonState = digitalRead(buttonPin);
+  Serial.println(time);
+  Serial.println(toggleState);
 
   // debounce + toggle
-  if (buttonState == HIGH && previous == LOW && millis() - time > debounce) 
+  // if (buttonState == HIGH && previous == LOW && millis() - time > debounce) 
+  if (buttonState == LOW && (millis() - time) > debounce) 
   {
-    if (state == HIGH)
+    if (toggleState == HIGH)
     {
-          // turn LED on:    
-      digitalWrite(ledPin, HIGH);  
       digitalWrite(buzzpin, HIGH);  
 
       for (int i = 0 ; i < activityLevel; i++)
@@ -115,28 +143,32 @@ void togglelights()
       {
         strip.setPixelColor(i, strip.Color(0,0,0));  
       }
-    
-      strip.show();
       
-      state = LOW; // toggle
+      Serial.println("yyy");
+
+      //strip.show();
+      
+      //toggleState = LOW; 
     }
     else
     {
-      // turn LED off:
-      digitalWrite(ledPin, LOW); 
       digitalWrite(buzzpin, LOW); 
     
       for (int i = 0 ; i < strip.numPixels(); i++)
       {
          strip.setPixelColor(i, strip.Color(0,0,0));
        }
-        strip.show();
+        //strip.show();
     
-      state = HIGH; // toggle
-    }
+      //toggleState = HIGH; 
+      Serial.println("XXXXXX");
 
+    }
+    
+    toggleState = !toggleState;
+    strip.show(); 
+    
     time = millis();    
   }
   
-  previous = buttonState;    
 }
